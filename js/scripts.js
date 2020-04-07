958af5fe8560f0d120678394c85faf9fb93e7b8e
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-04-07 15:47:45 -0700

    create HTML doc and add a form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Back-End Logic ------
 function Tickets(movieTitle, movieTime, movieAge) {
   this.movieTitle = movieName;
   this.movieTime = movieTime;
@@ -6,11 +7,18 @@ function Tickets(movieTitle, movieTime, movieAge) {
 }
 
 Ticket.prototype.ticketPrice = function() {
-  if(this.movieAge === 'senior' || this.movieAge === "kiddo") {
+  if(this.movieAge === 'senior' && this.movieTime === "morning" || this.movieAge === "kiddo" && this.movieTime === "morning") {
     this.price += 3;
+  }else if(this.movieAge === "senior" && this.movieTime === "night" || this.movieAge === "kiddo" && this.movieTime === "night") {
+    this.price +=4;
   }else{
-    this.price +=20;
+    this.price +=1000
   }
+  return this.price
 }
 
+
+
 // adult: $9.99 child/65+: $3
+
+// Front End Logic -------
